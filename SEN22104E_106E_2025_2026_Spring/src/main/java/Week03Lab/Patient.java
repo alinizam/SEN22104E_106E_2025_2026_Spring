5cc41849e30234db59c0d4e31a49d0186eaaa2b3
alinizam <alinizam99@gmail.com>
2026-02-26 13:13:58 +0300

    Week 03 Lab

---
+++ SEN22104E_106E_2025_2026_Spring/src/main/java/Week03Lab/Patient.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week03Lab;
+
+/**
+ *
+ * @author ali.nizam
+ */
+public class Patient {
+    String id;
+    String firstName, lastName,doctor;
+
+    public Patient(String id, String firstName, String lastName, String doctor) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.doctor = doctor;
+    }
+
+     
+    
+}
